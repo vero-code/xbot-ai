250f959a2c988b2f23d2c620a90ce68410c26dda
v_code <12273445-v_code@users.noreply.gitlab.com>
2025-02-11 12:05:43 +0200

    Initial

---
+++ src/main/java/org/example/xbotai/XbotAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.xbotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbotAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbotAiApplication.class, args);
+    }
+
+}
