c650850722d0ba82b9f15d7e275cfc89d1a227c9
v_code <12273445-v_code@users.noreply.gitlab.com>
2025-02-14 14:32:18 +0200

    Call methods from blockchain

---
+++ blockchain/near-logger.js
@@ -0,0 +1,54 @@
+const nearAPI = require('near-api-js');
+const { connect, keyStores } = nearAPI;
+const os = require('os');
+const homedir = os.homedir();
+
+const CONTRACT_NAME = 'xbotai.testnet';
+
+async function connectToNear() {
+    const keyStore = new keyStores.UnencryptedFileSystemKeyStore(`${homedir}/.near-credentials`);
+    const near = await connect({
+        networkId: 'testnet',
+        keyStore,
+        nodeUrl: 'https://rpc.testnet.near.org'
+    });
+
+    const account = await near.account(CONTRACT_NAME);
+
+    const contract = new nearAPI.Contract(account, CONTRACT_NAME, {
+        viewMethods: ['getLogs'],
+        changeMethods: ['addLog']
+    });
+
+    return { contract, account };
+}
+
+async function addLog(tweet) {
+    const { contract, account } = await connectToNear();
+    await contract.addLog({
+        signerAccount: account,
+        args: { tweet },
+        gas: '30000000000000',
+        amount: '0'
+    });
+    console.log('Tweet added to blockchain:', tweet);
+}
+
+async function getLogs() {
+    const { contract } = await connectToNear();
+    const logs = await contract.getLogs();
+    console.log('Logs from blockchain:', logs);
+    return logs;
+}
+
+module.exports = { addLog, getLogs };
+
+(async () => {
+    const args = process.argv.slice(2);
+    if (args.length > 0) {
+        await addLog(args[0]);
+    } else {
+        const logs = await getLogs();
+        console.log('Retrieved logs:', logs);
+    }
+})();
